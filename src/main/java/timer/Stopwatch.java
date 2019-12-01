40f6c28e3e765c249d9b52117fe447c9dafb9dff
Rafal Holowiak <rholowiak@gmail.com>
2019-12-01 09:53:17 +0100

    list + insert sort + stopwatch

---
+++ src/main/java/timer/Stopwatch.java
@@ -0,0 +1,14 @@
+package timer;
+
+public class Stopwatch {
+    private long startTime;
+    public void Start(){
+        startTime = System.currentTimeMillis();
+
+    }
+
+    public long Stop(){
+        return System.currentTimeMillis() - startTime;
+
+    }
+}
