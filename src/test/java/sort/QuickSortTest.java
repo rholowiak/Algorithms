ee8b0b1d4abf165cb06f0ca724207d47e81c5439
Rafal Holowiak <rholowiak@gmail.com>
2019-12-02 17:10:16 +0100

    test folder repair

--- src/test/java/sort/QuickSortTest.java
+++ src/test/java/sort/QuickSortTest.java
@@ -8,14 +8,15 @@ import java.util.Random;
 
 public class QuickSortTest {
     @Test
-    public void exampleDataShouldBeSorted(){
-        int[] table = new int[] {4,2,6,9,3,10,5};
+    public void exampleDataShouldBeSorted() {
+        int[] table = new int[]{4, 2, 6, 9, 3, 10, 5};
         QuickSort qs = new QuickSort();
         qs.sort(table, 0, table.length - 1);
 
         for (int i = 0; i < table.length - 1; i++) {
-            Assert.assertTrue(table[i] <= table[i+1]);
+            Assert.assertTrue(table[i] <= table[i + 1]);
         }
+    }
 
     @Test
     public void randomDataShouldBeSorted(){
